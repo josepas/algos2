5ead769e330edaa67ed5ceec856edcf3469ee91f
Jose Pascarella <josepas979@gmail.com>
2013-11-24 09:36:00 -0430

    termine con pila y colaop. tope(), desempilar(), vacia(), encolar(m)

--- proyecto2/cola.py
+++ proyecto2/cola.py
@@ -2,21 +2,25 @@ class cola:
     def __init__(self,m):
         self.cont = []
         self.tam = 0
-        self.MAX = m
         
     def vacia(self):
         return (self.tam == 0)
     
+    def tope(self):
+        if (self.tam > 0):
+            return self.cont[0]
+        print('Cola vacia')
+    
     def encolar(self, nodo):
-        if (self.tam < self.MAX):
-            self.cont.insert(nodo)
+            self.cont.append(nodo)
             self.tam += 1
-        else:
-            print('La pila esta llena')
    
-    def desempilar(self):
+    def desencolar(self):
         if not self.vacia():
             self.tam -= 1
-            return self.cont.pop()
-        print('La pila esta vacia')
+            self.cont.remove(self.cont[0])
+            return
+        print('Cola vacia')
     
+    
+  
--- proyecto2/pila.py
+++ proyecto2/pila.py
@@ -2,21 +2,24 @@ class pila:
     def __init__(self,m):
         self.cont = []
         self.tam = 0
-        self.MAX = m
         
     def vacia(self):
         return (self.tam == 0)
     
+    def tope(self):
+        if (self.tam > 0):
+            return self.cont[-1]
+        print('Pila vacia')
+    
+    
     def empilar(self, nodo):
-        if (self.tam < self.MAX):
             self.cont.append(nodo)
             self.tam += 1
-        else:
-            print('La pila esta llena')
-   
+           
     def desempilar(self):
         if not self.vacia():
             self.tam -= 1
-            return self.cont.pop()
+            self.cont.remove(self.cont[-1])
+            return
         print('La pila esta vacia')
     
