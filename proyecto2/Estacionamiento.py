b8194e5ecb834b16c4f881b6e1a55240356b52cc
Amin Arria <arria.amin@gmail.com>
2013-11-27 20:36:38 -0430

    Listo init de Estacionamiento

--- proyecto2/Estacionamiento.py
+++ proyecto2/Estacionamiento.py
@@ -1,4 +1,10 @@
-import cola from cola
+from cola import cola
+from random import *
 
 class Estacionamiento(cola):
-    
+   i = 0
+   def __init__(self):
+      random.seed()
+      self.iden = Estacionamiento.i
+      Estacionamiento.i += 1 
+      super().__init__() #inicializa la cola
--- proyecto2/cola.py
+++ proyecto2/cola.py
@@ -1,5 +1,5 @@
 class cola:
-    def __init__(self,m):
+    def __init__(self):
         self.cont = []
    
     def Tope(self):
