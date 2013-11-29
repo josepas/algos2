a2b6339c42cd9860fca1a9ee422ec03e3a9f6660
Amin Arria <arria.amin@gmail.com>
2013-11-28 22:06:26 -0430

    Hice Cabe y Estacionar, las necesitaba

--- proyecto2/Tubo.py
+++ proyecto2/Tubo.py
@@ -4,34 +4,36 @@ import random
 random.seed()
 
 class Tubo(pila):
-   i = 0 
-   def __init__(self, iden=-1, c=-1):
-      if(iden == -1):
-         self.iden = Tubo.i
-         Tubo.i += 1
-      else:
-         self.iden = iden
-      
-      if(c == -1):
-         self.capacidad = random.randint(5,25)
-      else:
-         self.capacidad = c
+    i = 0 
+    def __init__(self, iden=-1, c=-1):
+        if(iden == -1):
+            self.iden = Tubo.i
+            Tubo.i += 1
+        else:
+            self.iden = iden
 
-      super().__init__() #inicializa la pila
-      self.ocupacion = 0
-      
-   def Cabe(self):
-      return None
+        if(c == -1):
+            self.capacidad = random.randint(5,25)
+        else:
+            self.capacidad = c
 
-   def Estacionar(self, v):
-      if (self.ocupacion < self.capacidad):
-         self.Empilar(v)
-         self.ocupacion += v.longitud
+        super().__init__() #inicializa la pila
+        self.ocupacion = 0
+
+    def Cabe(self, v):
+        ocupara = self.ocupacion + v.longitud
+        return (ocupara <= self.capacidad)
+
+    def Estacionar(self, v):
+        if (self.Cabe(v)):
+            self.Empilar(v)
+            self.ocupacion += v.longitud
 
     def Retirar(self, v):
-    
-    
+        return None
+
     def Cercano(self):
-	self.Tope
-    
+        self.Tope
+
     def Existe(self):
+        return None
