efa693441f47d7988c452978119650be1411cd56
Jose Pascarella <josepas979@gmail.com>
2013-12-07 10:32:05 -0430

    NI

---
+++ proyecto3/mergeSort.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Proyecto 3
+#
+# Algoritmo de Ordenamiento MergeSort 
+#
+# Autores:
+#   Jose Pascarella     11-10743
+#   Amin Arria          11-10053
+#
+# Ultima Modificacion: 19 / 12 / 2013
