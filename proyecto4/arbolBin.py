85d7c8e4efa20a4addf4cf7ecf9f3a3d580830e6
Jose Pascarella <josepas979@gmail.com>
2014-01-11 19:08:25 -0430

    creo que termina ADD

--- proyecto4/arbolBin.py
+++ proyecto4/arbolBin.py
@@ -1,22 +1,46 @@
 from nodo import baseN
 
 class arbolBin:
-    def __init__(self, padre):
-        self.info = 
-        self.padre = None
-        self.izq = None
-        self.der = None
+    def __init__(self, info, padre):
+        self.raiz = baseN(None, '')
         
     def procesar(self, archivo):
-        entrada = open( archivo, 'r')
+        entrada = open('casoPrueba.txt', 'r')
         for i in entrada:
             print(i[0], i[1], 'hola!')
             
     
+    def GET(self, clave):
     
-    def GET
     
     
-    def ADD
+    def ADD(self, nodo, clave):
+        act = clave[0]
+        if (act != 'A') or (act != 'T'):
+            nodo.cant += 1
+            return
+        
+        clave = clave[1:]
+        if act == 'A':
+            if nodo.izq == None:
+                nodo.izq == baseN(nodo, nodo.cadena + act)
+            self.ADD(nodo.izq, clave)
+        
+        if act == 'T':
+            if nodo.der == None:
+                nodo.der == baseN(nodo, nodo.cadena + act)
+            self.ADD(nodo.der, clave)
+        
+        
+        
+         
+            
+        
+                
     
-    def GETALL
+    
+    
+    
+    
+
+h = arbolBin(None, None)
--- proyecto4/nodo.py
+++ proyecto4/nodo.py
@@ -1,6 +1,8 @@
 class baseN:
-    def __init__(self, cant=0):
-        self.cant = cant
-        #self.base = 
-        
+    def __init__(self, padre, cadena):
+        self.padre =  
+        self.cadena =   
+        self.cant = 0
+        self.izq = None
+        self.der = None
         
