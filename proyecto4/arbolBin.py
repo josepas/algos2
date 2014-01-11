20290a7e2970402c996c0e1ec151cdb44877ddd3
Jose Pascarella <josepas979@gmail.com>
2014-01-11 17:38:48 -0430

    comienzo el proy4

---
+++ proyecto4/arbolBin.py
@@ -0,0 +1,22 @@
+from nodo import baseN
+
+class arbolBin:
+    def __init__(self, padre):
+        self.info = 
+        self.padre = None
+        self.izq = None
+        self.der = None
+        
+    def procesar(self, archivo):
+        entrada = open( archivo, 'r')
+        for i in entrada:
+            print(i[0], i[1], 'hola!')
+            
+    
+    
+    def GET
+    
+    
+    def ADD
+    
+    def GETALL
---
+++ proyecto4/nodo.py
@@ -0,0 +1,6 @@
+class baseN:
+    def __init__(self, cant=0):
+        self.cant = cant
+        #self.base = 
+        
+        
